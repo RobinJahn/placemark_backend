0e7f251f16c7d17c5d6e1ec77f341a6a5215c5a7
RobinJahn <robin@kamerapilot.com>
2023-06-09 10:21:54 +0200

    Added user api

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,1 +1,8 @@
-export const apiRoutes = [];
+import { userApi } from "./api/users-api.js";
+
+export const apiRoutes = [
+  { method: "GET", path: "/api/users", config: userApi.find },
+  { method: "POST", path: "/api/users", config: userApi.create },
+  { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
+  { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+];
